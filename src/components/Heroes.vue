a62e8c578a8641130edb32083405e9f8bab570d6
ebarredo <ebarredo@hiberus.com>
2021-12-14 18:47:15 +0100

    añade descripcion a los heroes

--- src/components/Heroes.vue
+++ src/components/Heroes.vue
@@ -2,13 +2,21 @@
     <div class="container">
         <div class="form-group">
             <label for>New Hero</label>
-            <input type="text" class="form-control" name id aria-describedby="helpId" placeholder v-model="newHero" />
+            <input type="text" class="form-control" name id aria-describedby="helpId" placeholder v-model="newHero.name" />
             <small id="helpId" class="form-text text-muted" v-if="newHero.length === 0" >hero name should not be empty</small>
+        </div>
+        <div class="form-group">
+            <label for>Hero Descripcion</label>
+            <input type="text" class="form-control" name id aria-describedby="helpId" placeholder v-model="newHero.description" />
+            <small id="helpId" class="form-text text-muted">(Optional)</small>
         </div>
         <button type="button" class="btn btn-primary" @click="addHero()" v-bind:disabled="newHero.length === 0">Add Hero</button>
 
         <ul class="list-group mt-3">
-            <li class="list-group-item" v-for="(hero, index) in heroes" :key="index">{{ hero }}</li>
+            <li class="list-group-item" v-for="(hero, index) in heroes" :key="index">
+               <span>{{hero.name}}</span> <br/>
+               <small>{{hero.description}}</small>
+            </li>
         </ul>
     </div>
 </template>
@@ -18,15 +26,19 @@ export default {
     name: 'Heroes',
     data() {
         return {
-            newHero: '',       
-            heroes: ['Spiderman', 'Ironman', 'Hulk', 'Thor']
+            newHero: {},       
+            heroes: [
+                {name:'Spiderman', description: 'Peter Parker'}, 
+                {name:'Ironman', description: 'Tony Stark'},
+                {name:'Hulk', description: 'Bruce Banner'},
+                {name:'Thor', description: 'Odin\'s son'}]
         }
     },
     methods: {
         addHero() {
-            if(this.newHero.length > 0) {
+            if(this.newHero.name.length > 0) {
                 this.heroes.push(this.newHero);
-                this.newHero = '';
+                this.newHero = {}
             }
         }
     }
